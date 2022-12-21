70b08d41ee1b08ff339d84ebad47b1ac5bec90e6
Duvi <evan.duvivier@icloud.com>
2022-12-22 00:43:40 +0100

    LOGOP_v0.3.0

--- js/script.js
+++ js/script.js
@@ -1,2 +1,36 @@
 //script.js
+//
+export const backToMenu = () => {
+	let homeButton = document.getElementById("home");
+	homeButton.addEventListener("click", function()  {
+		window.location.href = "/index.html";
+	});
+}
 
+export const goToLogo = () => {
+	let homeButton = document.getElementById("logo");
+	homeButton.addEventListener("click", function()  {
+		window.location.href = "/logo.html";
+	});
+}
+
+export const goToFormules = () => {
+	let homeButton = document.getElementById("formules");
+	homeButton.addEventListener("click", function()  {
+		window.location.href = "/formules.html";
+	});
+}
+
+export const goToAbout = () => {
+	let homeButton = document.getElementById("about");
+	homeButton.addEventListener("click", function()  {
+		window.location.href = "/about.html";
+	});
+}
+
+export const goToAvis = () => {
+	let homeButton = document.getElementById("avis");
+	homeButton.addEventListener("click", function()  {
+		window.location.href = "/avis.html";
+	});
+}
